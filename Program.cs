2e18f1fc9d5bfd54f554c3e14394ae380bdd8f4b
erdur <erdur@CNOBY>
2024-11-20 23:50:18 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace mükemmel_sayı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Mükemmel Sayı İçin Değer Giriniz ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            for (int i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    Console.WriteLine( toplam = toplam + i);
+
+                }
+
+            }
+            if (toplam == sayi)
+            {
+                Console.WriteLine(toplam);
+            }
+            else 
+            {
+                Console.WriteLine("Sayı Mükemmel Değil");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
